00e8dbe93934d9547f6ca99f5543681c18683fa2
luizh-ssousa <luizh.ssousa@gmail.com>
2024-12-19 23:37:31 -0300

    conversor em dolar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 const convertButton = document.querySelector(".convert-button")
 
 function convertValues(){
-    console.log("Funcionow")
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    console.log(convertedValue)
 }
 
 
