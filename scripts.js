2cbb6ae942ed51d754474d6578b84411434e0e28
luizh-ssousa <luizh.ssousa@gmail.com>
2024-12-19 22:59:40 -0300

    teste botão

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    console.log("Funcionow")
+}
+
+
+convertButton.addEventListener("click", convertValues)
