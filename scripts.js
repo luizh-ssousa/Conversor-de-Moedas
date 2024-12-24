1e0cf15c56b93676f675c5ea41d667390aa2afe5
luizh-ssousa <luizh.ssousa@gmail.com>
2024-12-23 21:32:59 -0300

    pegando valores pelo js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") // Outras Moedas
     
+    console.log(currencySelect.value)
     const dolarToday = 5.2
+    const euroToday = 6.2
 
     const convertedValue = inputCurrencyValue / dolarToday
 
